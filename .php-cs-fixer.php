6666b1d9cceb19438239a7495b5fa0ddc078d17f
Junior Grossi <d24a33f@eifht.com>
2026-06-06 14:56:41 +0100

    add cs fix

---
+++ .php-cs-fixer.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12'                    => true,
+        'strict_param'              => true,
+        'declare_strict_types'      => true,
+        'array_syntax'              => ['syntax' => 'short'],
+        'ordered_imports'           => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports'         => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true);
